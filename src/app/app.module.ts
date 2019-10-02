69e5a8a05272dfa8391dd05f1f708178db6be64c
Cristian Budzicz <cbudzicz@santafe.gov.ar>
2019-10-02 17:12:48 -0300

    Creado componente directivas,agregadas modificaciones en los htmls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DirectivasComponent } from './directivas/directivas.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    DirectivasComponent
   ],
   imports: [
     BrowserModule,
