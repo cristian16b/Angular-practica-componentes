dc6ecc435b0ed357a16234c6ed62cde50a786c33
Cristian Budzicz <cbudzicz@santafe.gov.ar>
2019-10-04 17:22:03 -0300

    Logrado cambiar estilo css con render2,to-do seguir investigando

--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-render',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RenderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
-
+  
+  cambiarColor(elemento:HTMLElement) {
+    this.renderer.addClass(elemento, 'colorTextoModificado');
+  }
 }
