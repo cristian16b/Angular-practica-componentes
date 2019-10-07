c516c17496c95c4bcf92456b2d5bbc18b5afbd7c
Cristian Budzicz <cbudzicz@santafe.gov.ar>
2019-10-07 16:29:52 -0300

    Logrado combiar eventos con viewchild,remove y add class

--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Renderer2 } from '@angular/core';
+import { Component, OnInit , Renderer2 , ViewChild , ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-render',
@@ -6,13 +6,22 @@ import { Component, OnInit , Renderer2 } from '@angular/core';
   styleUrls: ['./render.component.css']
 })
 export class RenderComponent implements OnInit {
-
+  //
+  @ViewChild('miparrafo',{static: false}) parrafo: ElementRef;
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
   
+  // al clickear sobre el parrafo, se le cambia el color
   cambiarColor(elemento:HTMLElement) {
     this.renderer.addClass(elemento, 'colorTextoModificado');
+  }
+
+  // al clickear sobre el boton, se retorna al color original del parrafo 
+  retornarColor() {
+    // console.log('evento');
+    this.renderer.removeClass(this.parrafo.nativeElement,'colorTextoModificado');
+    this.renderer.addClass(this.parrafo.nativeElement,'colorTextoOriginal');
   }
 }
