244403e63ea3c7d511682f0d3949c5ecff13f3d6
Cristian Budzicz <cbudzicz@santafe.gov.ar>
2019-10-07 17:30:39 -0300

    Agregado gestion evento mouseover, eliminar y agregar atributos

--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit , Renderer2 , ViewChild , ElementRef } from '@angular
 export class RenderComponent implements OnInit {
   //
   @ViewChild('miparrafo',{static: false}) parrafo: ElementRef;
+  @ViewChild('botonNoHaceNada',{static: false}) botonNoHaceNada: ElementRef;
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
@@ -16,6 +17,9 @@ export class RenderComponent implements OnInit {
   // al clickear sobre el parrafo, se le cambia el color
   cambiarColor(elemento:HTMLElement) {
     this.renderer.addClass(elemento, 'colorTextoModificado');
+    // vamos a bloquear el boton <no hago nada>
+    // todo solo funciona la primera vez
+    this.renderer.setAttribute(this.botonNoHaceNada.nativeElement, "disabled", "true");
   }
 
   // al clickear sobre el boton, se retorna al color original del parrafo 
@@ -23,5 +27,13 @@ export class RenderComponent implements OnInit {
     // console.log('evento');
     this.renderer.removeClass(this.parrafo.nativeElement,'colorTextoModificado');
     this.renderer.addClass(this.parrafo.nativeElement,'colorTextoOriginal');
+    // remuevo el disabled y agrego el enabled
+    this.renderer.removeAttribute(this.botonNoHaceNada.nativeElement,'disabled');
+    this.renderer.setAttribute(this.botonNoHaceNada.nativeElement, "enabled", "true");
+    console.log('fin');
+  }
+  
+  molestoAlert() {
+    alert('soy un molesto alert y este boton no hace nada');
   }
 }
