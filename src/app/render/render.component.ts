7107ae281c55a5d3b2baceae751ca0a357d0c44b
Cristian Budzicz <cbudzicz@santafe.gov.ar>
2019-10-08 16:21:50 -0300

    Se agregaron metodos createElement,appendChild y una variable contador tipo number

--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -9,9 +9,13 @@ export class RenderComponent implements OnInit {
   //
   @ViewChild('miparrafo',{static: false}) parrafo: ElementRef;
   @ViewChild('botonNoHaceNada',{static: false}) botonNoHaceNada: ElementRef;
+  @ViewChild('divLista',{static: false}) divLista: ElementRef;
   constructor(private renderer: Renderer2) { }
+  // variable contador que cuenta cuantas veces se presiono el boton <Cambiar de color>
+  contador : number;
 
   ngOnInit() {
+    this.contador = 0;
   }
   
   // al clickear sobre el parrafo, se le cambia el color
@@ -20,6 +24,17 @@ export class RenderComponent implements OnInit {
     // vamos a bloquear el boton <no hago nada>
     // todo solo funciona la primera vez
     this.renderer.setAttribute(this.botonNoHaceNada.nativeElement, "disabled", "true");
+    // creo otro elemento del tipo input
+    let li = this.renderer.createElement('li');
+    let span = this.renderer.createElement('span');
+    let text = this.renderer.createText(`Se apreto el boton ${this.contador} veces`);
+    this.renderer.appendChild(span, text);
+    this.renderer.appendChild(li, span);
+    this.renderer.appendChild(this.divLista.nativeElement,li);
+    // this.renderer.appendChild(this.li.nativeElement,li);
+    // this.renderer.appendChild(this.li.nativeElement,this.renderer.createText('cambio texto'))
+    //actualizo el contador
+    this.contador = this.contador + 1;
   }
 
   // al clickear sobre el boton, se retorna al color original del parrafo 
@@ -30,7 +45,9 @@ export class RenderComponent implements OnInit {
     // remuevo el disabled y agrego el enabled
     this.renderer.removeAttribute(this.botonNoHaceNada.nativeElement,'disabled');
     this.renderer.setAttribute(this.botonNoHaceNada.nativeElement, "enabled", "true");
-    console.log('fin');
+    // console.log('fin');
+    this.renderer.removeAttribute(this.botonNoHaceNada.nativeElement,'value');
+    this.renderer.setAttribute(this.botonNoHaceNada.nativeElement,'value','AHORA TENGO OTRO NOMBRE');
   }
   
   molestoAlert() {
